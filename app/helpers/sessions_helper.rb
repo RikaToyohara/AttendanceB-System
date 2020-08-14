b766e82f74843c5083853b11a97f8dfe32f3ae8a
RikaToyohara <rikachan814sun@gmail.com>
2020-08-14 12:37:27 +0000

    ヘッダードロップダウンメニュー設定

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,8 +6,13 @@ module SessionsHelper
   
   # 現在ログイン中のユーザーがいる場合オブジェクトを返す
   def current_user
-      if session[:user_id]
-        @current_user || User.find_by(id: session[:user_id])
-      end
+    if session[:user_id]
+      @current_user || User.find_by(id: session[:user_id])
+    end
+  end
+  
+  # 現在ログイン中のユーザーがいればtrue でなければfalseを返す
+  def logged_in?
+    !current_user.nil?
   end
 end
