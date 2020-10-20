a7bff630b1a422903bdc86e7c277bfdc8698538a
RikaToyohara <rikachan814sun@gmail.com>
2020-10-20 22:34:32 +0000

    ヘッダー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :edit_all, :update_basic_info]
   before_action :logged_in_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: [:index, :destroy, :edit_basic_info, :update_basic_info]
+  before_action :admin_user, only: [:index, :destroy, :edit_basic_info, :edit_all, :update_basic_info]
   before_action :set_one_month, only: :show
   
   def index
@@ -48,6 +48,9 @@ class UsersController < ApplicationController
   end
   
   def edit_basic_info
+  end
+  
+  def edit_all
   end
   
   def update_basic_info
